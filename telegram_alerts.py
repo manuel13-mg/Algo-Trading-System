a2c0d1a24c2f8743b1a5e63578c3992fe1be5a7f
manuel13-mg <63940020+manuel13-mg@users.noreply.github.com>
2025-06-26 09:45:42 +0530

    Add files via upload

---
+++ telegram_alerts.py
@@ -0,0 +1,14 @@
+import requests
+TELEGRAM_TOKEN = '7946617060:AAEMb0gTrBfpzoN-Dc8KqDNH443dEwu864U'
+TELEGRAM_CHAT_ID = '1411283657'
+
+def send_telegram_message(message):
+    url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
+    data = {'chat_id': TELEGRAM_CHAT_ID, 'text': message}
+    response = requests.post(url, data=data)
+
+    if response.status_code == 200:
+        print(f"Telegram message sent successfully: {message}")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+
